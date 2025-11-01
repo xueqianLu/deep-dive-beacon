1dfbd6285d48bbf3f6fceda10e4ec6391eddbea7
luxq <xueqian1991@gmail.com>
2025-11-01 23:14:34 +0800

    add deploy

--- cmd/deploy/production/prod.go
+++ cmd/deploy/production/prod.go
@@ -4,6 +4,8 @@ import (
 	"github.com/redis/go-redis/v9"
 	"github.com/sirupsen/logrus"
 	"github.com/xueqianLu/deep-dive-beacon/config"
+	"github.com/xueqianLu/deep-dive-beacon/constant"
+	"github.com/xueqianLu/deep-dive-beacon/models/dbmodels"
 	"github.com/xueqianLu/deep-dive-beacon/types"
 	"gorm.io/gorm"
 )
@@ -27,5 +29,17 @@ func NewProdDeploy(db *gorm.DB, redis *redis.Client, logger *logrus.Logger, cfg 
 }
 
 func (d ProdDeploy) Execute() error {
+	if d.depcfg.BlockScan != nil {
+		d.addScanTask(d.depcfg.BlockScan)
+	}
 	return nil
+}
+
+func (d ProdDeploy) addScanTask(scan *types.BlockScanTask) error {
+	task := &dbmodels.ScanTask{
+		TaskType:   constant.SCAN_TYPE_BEACON_BLOCK,
+		LastNumber: scan.Start,
+		Enabled:    true,
+	}
+	return d.db.Create(task).Error
 }
--- types/deployconfig.go
+++ types/deployconfig.go
@@ -1,4 +1,8 @@
 package types
 
+type BlockScanTask struct {
+	Start uint64 `json:"start"`
+}
 type DeployConfig struct {
+	BlockScan *BlockScanTask `json:"block_scan"`
 }
